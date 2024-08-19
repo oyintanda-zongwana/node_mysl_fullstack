5e77e63e2e8d1001e561bb48e84faabbbc7b4add
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-08-19 09:48:32 +0200

    port added

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ const pool = createPool({
     host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
+    port: process.env.PORT,
     password: process.env.PASSWORD
 });
 
